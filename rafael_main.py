6fb9c42d346528db0d39bf0c4c6acec790ded887
madhavjai <madhav8415@ggn.hxls.org>
2026-08-24 15:04:44 +0530

    Update rafael_main.py

--- rafael_main.py
+++ rafael_main.py
@@ -1,8 +1,5 @@
-from rafael_robot import left_motor, right_motor
-from rafael_motion import set_motor_speed, stop, drive_straight
-from pybricks.tools import wait
-from rafael_odometry import print_position
+from rafael_field_map import calibrate
+import rafael_state_machine as state_machine
 
-drive_straight(500)
-
-print_position()
+calibrate()
+state_machine.run()
