1597898f29b34d69055382e563745182a5486a0f
madhavjai <madhav8415@ggn.hxls.org>
2026-08-24 13:18:44 +0530

    Update rafael_robot.py
    added actual ports

--- rafael_robot.py
+++ rafael_robot.py
@@ -3,7 +3,7 @@ Robot hardware.
 """
 
 from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, UltrasonicSensor
+from pybricks.pupdevices import Motor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Port
 
 hub = PrimeHub()
@@ -12,6 +12,8 @@ left_motor = Motor(Port.A)
 
 right_motor = Motor(Port.E)
 
-distance_sensor = UltrasonicSensor(Port.D)
+distance_sensor = UltrasonicSensor(Port.B)
+
+touch_sensor = ForceSensor(Port.D)
 
 gyro = hub.imu
